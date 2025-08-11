643e4328b9892bb111ff548d3c53b5f719cb051f
Shofian <piaanjack@gmail.com>
2025-08-11 17:27:15 +0700

    initial commit

---
+++ src/test/java/Selenium.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Selenium {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup(); // Setup driver
+        driver = new ChromeDriver(); // Create driver instance
+    }
+
+    @Test
+    public void helloWorld() {
+        driver.get("https://jayjay.co");
+
+        String text = driver.findElement(By.className("first-course")).getText();
+
+        assertEquals("Gebrakan Kursus Online yang Siap Meroketkan Kariermu", text);
+    }
+}
